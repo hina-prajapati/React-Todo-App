2e219c456dc7a4507ef31e8167e8e71e490f14d5
anchal <anchal@gmail.com>
2025-07-15 13:52:53 +0530

    grrentick data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ThemeLog from './ThemeLog'; // ✅ Step 1: Import the component
 import Form from './Form';
 import TodoApp from './TodoApp';
@@ -9,22 +9,55 @@ import Practice from './practice';
 import HandleForm from './form2';
 
 export default function App() {
-  const [color, setColor] = useState("white");
 
-  const toggleBtn = () =>{
-    setColor(color === "white" ? "black" : "white");
+  const [article, setArticle] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchApi = async () => {
+    try {
+      setLoading(true);
+      let res = await fetch('https://greentick.taxsutra.com/api/get-expert-article');
+      let data = await res.json();
+      console.log(data);
+      console.log(data.img, data.thumbnail);
+
+      setArticle(data);
+    } catch {
+      alert("error");
+    } finally {
+      setLoading(false);
+    }
   }
 
-  const isDarkMode = color === "black";
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
 
   return (
-    <div style={{ backgroundColor: color, color: isDarkMode ? "white" : "black", height: "100vh", padding: "20px", width:400 }}>
-    <button onClick={toggleBtn} style={{fontSize:30}}>
-     { isDarkMode ? <FaMoon style={{color:"white"}} /> : <FaSun style={{color:"yellow"}} /> }
-    </button>
-    {/* <Form /> */}
-    {/* <TodoApp /> */}
-    <HandleForm />
+    <div style={{}}>
+      <p>{loading ? "Loading" : ""}</p>
+      <ul>
+        {article.map((item, index) => (
+
+          <li key={index}>
+            <p><strong>ID:</strong> {item.expert_article_id}</p>
+            <p><strong>Title:</strong> {item.topic_title}</p>
+             <p><strong>Name of Author:</strong> {item.author_data?.[0]?.name || 'N/A'}</p>
+            <p><strong>News Type:</strong>{item.author_data?.[0]?.affiliations || 'N/A'}</p>
+
+            <img
+              src={
+                item.author_data?.[0]?.url
+                  ? `https://greentick.taxsutra.com/upload/authors/${item.author_data[0].url}`
+                  : 'https://via.placeholder.com/150'
+              }
+              alt="Author"
+              style={{ width: 100, height: 'auto', borderRadius: 8, marginTop: 10 }}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
